e159ce356918e6b41c7eaea69f6826661e6a6bbd
Nivo Tech <nivotech.dev@gmail.com>
2026-01-21 15:53:54 +0000

    deixe a imagem da logo maior, mas nao mude o tamanho do header

--- src/components/quantix/Logo.tsx
+++ src/components/quantix/Logo.tsx
@@ -6,8 +6,8 @@ export function QuantixLogo({ className }: { className?: string }) {
       <Image 
         src="https://whfdrrdozhyavyflgaxo.supabase.co/storage/v1/object/sign/AgenciaQuantix/WhatsApp_Image_2026-01-20_at_15.00.50-removebg-preview.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9jOWY1OGE2Ni03MDFhLTRhNDQtOWJhZC0zOGRkNmVkYTNhODAiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJBZ2VuY2lhUXVhbnRpeC9XaGF0c0FwcF9JbWFnZV8yMDI2LTAxLTIwX2F0XzE1LjAwLjUwLXJlbW92ZWJnLXByZXZpZXcucG5nIiwiaWF0IjoxNzY5MDEwMDI5LCJleHAiOjIwODQzNzAwMjl9.hD67UWQYJZ7S3_Vov6MIBCcszI2US08l4qYmxsgiAFo" 
         alt="Quantix Logo" 
-        width={50} 
-        height={50} 
+        width={100} 
+        height={100} 
         className="text-primary" 
       />
       <div>
